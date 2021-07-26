96a824fd20cdcdbb1ab8b1d959c27c64e62b511e
aoki <ringohub@gmail.com>
2021-07-26 10:32:35 +0900

    Update Simpleget

---
+++ server/server.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httputil"
+
+	"github.com/k0kubun/pp"
+)
+
+func handlerDigest(w http.ResponseWriter, r *http.Request) {
+	pp.Println("URL: %s\n", r.URL.String())
+	pp.Println("Query: %v\n", r.URL.Query())
+	pp.Println("Proto: %s\n", r.Proto)
+	pp.Println("Method: %s\n", r.Method)
+	pp.Println("Header: %v\n", r.Header)
+
+	defer r.Body.Close()
+	body, _ := ioutil.ReadAll(r.Body)
+	fmt.Printf("--body--\n%s\n", string(body))
+	if _, ok := r.Header["Authorization"]; !ok {
+		w.Header().Add("WWW-Authenticate", `Digest realm="Secret Zone", nonce="TgLc25U2BQA=f510fkjwi3aw3jfaw3r3ra", algorithm=MD5, qop="auth"`)
+		w.WriteHeader(http.StatusUnauthorized)
+	} else {
+		fmt.Fprintf(w, "<html><body>secret page</body></html>\n")
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Set-Cookie", "VISIT=TRUE")
+	pp.Println("Query: %v\n", r.URL.Query())
+
+	dump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(string(dump))
+	if _, ok := r.Header["Cookie"]; ok {
+		fmt.Fprintf(w, "<html><body>2回目以降</body></html>\n")
+	} else {
+		fmt.Fprintf(w, "<html><body>hello 初訪問</body></html>\n")
+	}
+}
+
+func main() {
+	var httpServer http.Server
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/digest", handlerDigest)
+	log.Println("start http listening :80")
+	httpServer.Addr = ":80"
+	log.Println(httpServer.ListenAndServe())
+}
--- simpleget/main.go
+++ simpleget/main.go
@@ -4,10 +4,14 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 func main() {
-	resp, err := http.Get("http://localhost")
+	values := url.Values{
+		"query": {"hello world"},
+	}
+	resp, err := http.Get("http://localhost" + "?" + values.Encode())
 	if err != nil {
 		panic(err)
 	}
